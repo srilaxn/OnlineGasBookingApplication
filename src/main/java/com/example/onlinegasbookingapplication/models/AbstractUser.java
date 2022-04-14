6e03b6b7b652738a29a9e0f7b1d829b1a7a3cdf4
Tanishq Mishra <tanishq.189303036@muj.manipal.edu>
2022-04-14 11:22:19 +0530

    Initial commit with AbstractUser and Admin

---
+++ src/main/java/com/example/onlinegasbookingapplication/models/AbstractUser.java
@@ -0,0 +1,60 @@
+package com.example.onlinegasbookingapplication.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Inheritance(strategy= InheritanceType.TABLE_PER_CLASS)
+public abstract class AbstractUser {
+    String username;
+    String password;
+    String address;
+    String mobileNumber;
+    String email;
+
+    public AbstractUser() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/example/onlinegasbookingapplication/models/Admin.java
@@ -0,0 +1,25 @@
+package com.example.onlinegasbookingapplication.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Admin extends AbstractUser{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_id", nullable = false)
+    private Long adminId;
+
+    public Admin() {
+
+    }
+
+    public Long getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Long adminId) {
+        this.adminId = adminId;
+    }
+
+
+}
